8d8b02f943b179ed7dd2331c13bb0534b89ed156
zigos88 <96542132+zigos88@users.noreply.github.com>
2022-03-08 15:45:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balans = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = balans + transfer;
+        System.out.println(total);
+    }
+}
